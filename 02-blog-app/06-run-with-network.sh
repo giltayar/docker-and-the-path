fe5ea48305c597024d4db17f9e420fde8c3c4e78
Gil Tayar <gil@tayar.org>
2018-06-14 16:43:59 +0300

    small fix

--- 02-blog-app/06-run-with-network.sh
+++ 02-blog-app/06-run-with-network.sh
@@ -1,4 +1,4 @@
 docker network create mynet
 
 docker run -d --network=mynet --name=mongo mymongo
-docker run -t -p 3000:3000 --network=mynet -e SECRET=shhhh -e MONGODB_URI=mongo:27017 myblog
+docker run -d -t -p 3000:3000 --network=mynet -e SECRET=shhhh -e MONGODB_URI=mongo:27017 myblog
